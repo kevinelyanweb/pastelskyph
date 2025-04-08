f2e23efa2895982c99bd9fb32ff68cdce4b5b7d4
KevinElyan <kevinelyanwork@gmail.com>
2025-04-08 17:25:20 +0700

    a
    akini

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
-// next.config.js
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
-  }
 };
 
-export default nextConfig;
+module.exports = nextConfig;
