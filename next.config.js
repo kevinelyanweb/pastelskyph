26398029bafd372096e6a3ea5186508237b2dd26
KevinElyan <kevinelyanwork@gmail.com>
2025-04-08 17:20:42 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+// next.config.js
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname);
+    return config;
+  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
